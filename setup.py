319048b01836c300f028f256a7bf49735fa47ff2
Christina Ludwig <christina.ludwig@uni-heidelberg.de>
2021-02-03 15:31:21 +0100

    update setup.py

--- setup.py
+++ setup.py
@@ -1,10 +1,22 @@
-from distutils.core import setup
+import setuptools
 
-setup(
-    name='ohsomepy',
-    version='0.1',
-    packages=['ohsome'],
-    author='Christina Ludwig',
-    author_email='christina.ludwig@uni-heidelberg.de'
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="ohsome",
+    version="0.9.0",
+    author="Christina Ludwig",
+    author_email="christina.ludwig@uni-heidelberg.de",
+    description="Python client for the ohsome API",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/GIScience/ohsome-py",
+    packages=["ohsome"],
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: GPL v3.0 License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires=">=3",
 )
-
