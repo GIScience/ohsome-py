8a36c9d494f34cf86f894409a37e240d5081268a
Julian Psotta <julian@openrouteservice.org>
2021-03-01 12:02:05 +0100

    global timeout of 180 seconds

--- ohsome/clients.py
+++ ohsome/clients.py
@@ -146,7 +146,7 @@ class _OhsomePostClient:
         filter2=None,
         format=None,
         showMetadata=None,
-        timeout=60,
+        timeout=180,
         groupByKey=None,
         groupByKeys=None,
         groupByValues=None,
