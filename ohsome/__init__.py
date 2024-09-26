31881efd02d9d333228eef9b6aeefcec8053e8ff
Moritz Schott <moritz.schott@heigit.org>
2024-09-26 15:22:53 +0200

    fix: import order in __init__.py is not arbitrary

--- ohsome/__init__.py
+++ ohsome/__init__.py
@@ -3,6 +3,7 @@
 
 """Ohsome API client for Python"""
 
-from .clients import OhsomeClient  # noqa
+# The order of imports here must remain to prevent circular imports
 from .exceptions import OhsomeException  # noqa
 from .response import OhsomeResponse  # noqa
+from .clients import OhsomeClient  # noqa
